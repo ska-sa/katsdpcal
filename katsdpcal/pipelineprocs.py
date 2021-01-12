fd8037380d7672cf578d656978e648baf5b5e4e7
KimMcAlpine <kim.mcalpine@gmail.com>
2021-01-12 16:32:03 +0200

    Address review comments

--- katsdpcal/pipelineprocs.py
+++ katsdpcal/pipelineprocs.py
@@ -90,8 +90,6 @@ def comma_list(type_):
         if value == '':
             return []
         parts = value.split(',')
-        if len(parts) == 1:
-            return type_(parts[0])
         return [type_(part.strip()) for part in parts]
 
     return convert
@@ -142,12 +140,12 @@ USER_PARAMS_CHANS = [
 
 # Parameters that the user can set directly, in units of Hz/MHz
 USER_PARAMS_FREQS = [
-    Parameter('k_bfreq', 'start frequency for k fit per band, (MHz)', comma_list(float)),
-    Parameter('k_efreq', 'stop frequency for k fit per band, (MHz)', comma_list(float)),
-    Parameter('g_bfreq', 'start frequency for g fit per band, (MHz)', comma_list(float)),
-    Parameter('g_efreq', 'stop frequency for g fit per band, (MHz)', comma_list(float)),
+    Parameter('k_bfreq', 'start frequency for k fit per subband, (MHz)', comma_list(float)),
+    Parameter('k_efreq', 'stop frequency for k fit per subband, (MHz)', comma_list(float)),
+    Parameter('g_bfreq', 'start frequency for g fit per subband, (MHz)', comma_list(float)),
+    Parameter('g_efreq', 'stop frequency for g fit per subband, (MHz)', comma_list(float)),
     Parameter('band_start_freq', 'start frequency range corresponding to parameters'
-              ' supplied per frequency band, (MHz)', comma_list(float), telstate=False),
+              ' supplied per frequency subband, (MHz)', comma_list(float), telstate=False),
     Parameter('rfi_average_hz', 'amount to average in frequency before flagging, (Hz)', float),
     Parameter('rfi_windows_post_average',
               'size of windows for SumThreshold on frequency averaged data, (channels)',
@@ -433,9 +431,8 @@ def parameters_for_freq(parameters, channel_freqs):
     """
     for key in parameters.keys():
         if key.endswith('_efreq') or key.endswith('_bfreq'):
-            if type(parameters[key]) == list:
+            if len(parameters[key]) > 1:
                 try:
-                    parameters['band_start_freq']
                     band_idx = max([i for i, s_freq in enumerate(parameters['band_start_freq'])
                                     if min(channel_freqs/1e6) >= s_freq])
                     parameters[key] = parameters[key][band_idx]
@@ -443,6 +440,9 @@ def parameters_for_freq(parameters, channel_freqs):
                     logger.error("If '%s' is a list of values,"
                                  " a 'band_start_freq' parameter is required", key)
                     raise
+            elif len(parameters[key]) == 1:
+                parameters[key] = float(parameters[key][0])
+
     # remove parameter as it is no longer required
     if 'band_start_freq' in parameters:
         del parameters['band_start_freq']
